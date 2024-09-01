73f2b48a904e38a8c567edcb1fe977ae085e0387
awesomeness674 <86088545+awesomeness674@users.noreply.github.com>
2024-09-01 09:04:07 -0700

    Delete js directory

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 50) {
-        $('.navbar-scroll').addClass('navbar-scrolled');
-    } else {
-        $('.navbar-scroll').removeClass('navbar-scrolled');
-    }
-});
