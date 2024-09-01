4e2a984318b1aa1daeba0c9bfce943000da5b277
awesomeness674 <86088545+awesomeness674@users.noreply.github.com>
2024-09-01 09:09:02 -0700

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar-scroll').addClass('navbar-scrolled');
+    } else {
+        $('.navbar-scroll').removeClass('navbar-scrolled');
+    }
+});
